72bd15849f816f0f7f022b44382e9e6bacdc4968
zeynab <zeynabjahanara80@gmail.com>
2025-10-31 16:32:34 +0330

    fix server

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,11 +1,13 @@
 const mongoose = require('mongoose');
+
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI);
-    console.log('MongoDB connected');
-  } catch (err) {
-    console.error('MongoDB connection error:', err);
+    const conn = await mongoose.connect(process.env.MONGO_URI);
+    console.log(` MongoDB connected successfully: ${conn.connection.host}`);
+  } catch (error) {
+    console.error(` MongoDB connection failed: ${error.message}`);
     process.exit(1);
   }
 };
+
 module.exports = connectDB;
--- backend/server.js
+++ backend/server.js
@@ -1,69 +1,3 @@
-// const express = require('express');
-// const dotenv = require('dotenv');
-// const connectDB = require('./config/db');
-// const cors = require('cors');
-// const commentRoutes = require('./routes/comment');
-// const ticketRoutes = require('./routes/ticket');
-// const userRoutes = require('./routes/user');
-// const path = require('path');
-// const articleRoutes = require('./routes/article');
-// const uploadRoutes = require('./routes/upload');
-// const userFavoritesRoutes = require('./routes/userFavorites');
-// const campaignRoute = require('./routes/campaign');
-// const mongoose = require('mongoose');
-// const Product = require('./models/Product');
-// const Category = require('./models/Category');
-// const statsRoutes = require("./routes/stats");
-// const userCartRoutes = require('./routes/userCart'); 
-// dotenv.config();
-// connectDB();
-
-// mongoose.connect(process.env.MONGO_URI)
-//   .then(() => console.log("✅ MongoDB Atlas connected successfully"))
-//   .catch(err => console.error("❌ MongoDB connection error:", err));
-
-// const app = express();
-
-// app.use(cors());
-// app.use(express.json());
-
-// app.use(express.urlencoded({ extended: true }));
-
-// app.use("/api/stats", statsRoutes);
-// app.use('/api/auth', require('./routes/auth'));
-// app.use('/api/categories', require('./routes/categories'));
-// // app.use('/api/cart', require('./routes/cart'));
-// app.use('/api/discounts', require('./routes/discounts'));
-// app.use('/api/products', require('./routes/product'));
-// app.use('/api/comments', commentRoutes);
-// app.use('/api/tickets', ticketRoutes);
-// app.use('/api/users', userRoutes);
-// app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
-// // app.use('/uploads', express.static('/uploads'));
-// app.use('/api/articles', articleRoutes);
-// app.use('/', uploadRoutes);
-// // app.use('/api/upload', uploadRoutes);
-// // app.use('/uploads', uploadRoutes);
-// app.use('/api/comment-articles', require('./routes/commentArticle'));
-// app.use('/api/user-favorites', userFavoritesRoutes);
-// app.use('/api/user/cart', userCartRoutes);
-// app.use('/api/campaign', campaignRoute);
-// const PORT = process.env.PORT || 5000;
-// app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
-
-// // ===============================
-// // Serve frontend build files
-// // ===============================
-// // const path = require('path');
-
-// app.use(express.static(path.join(__dirname, 'frontend')));
-
-
-// app.get('*', (req, res) => {
-//   res.sendFile(path.join(__dirname, '../frontend', 'index.html'));
-// });
-
-
 const express = require('express');
 const dotenv = require('dotenv');
 const connectDB = require('./config/db');
@@ -85,9 +19,9 @@ const userCartRoutes = require('./routes/userCart');
 dotenv.config();
 connectDB();
 
-mongoose.connect(process.env.MONGO_URI)
-  .then(() => console.log("✅ MongoDB Atlas connected successfully"))
-  .catch(err => console.error("❌ MongoDB connection error:", err));
+// mongoose.connect(process.env.MONGO_URI)
+//   .then(() => console.log("✅ MongoDB Atlas connected successfully"))
+//   .catch(err => console.error("❌ MongoDB connection error:", err));
 
 const app = express();
 const PORT = process.env.PORT || 5000;
